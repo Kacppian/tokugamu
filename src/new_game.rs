59695615fc96c55acff85f3d5174a737d012e145
Kaushik ASP <aspkaushik@gmail.com>
2018-12-08 23:11:29 +0530

    Added basic io functions

--- src/new_game.rs
+++ src/new_game.rs
@@ -1,3 +1,36 @@
+use std::io;
+use std::io::Write;
+
 pub fn start_game() -> () {
     println!("The game will start in ....");
+    loop {
+        read_input();
+    }
+}
+
+fn read_input() -> () {
+    print!("Outgoing<-");
+    std::io::stdout().flush().expect("flush failed. -- Something went wrong --");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("failed to read from stdin");
+    let trimmed = input.trim();
+    if trimmed == "qc" {
+        end_game();
+    }
+    else if trimmed == "q" {
+        quit_app();
+    }
+    else {
+        println!("Incoming->okay");
+    }
+}
+
+fn quit_app() -> () {
+    std::process::exit(0);
+}
+
+fn end_game() -> () {
+    println!("Are you sure ?");
 }
