a4bc7cc64728fbe9a7e074b4ced5c42e7ff285d4
Kaushik ASP <aspkaushik@gmail.com>
2018-12-08 21:25:31 +0530

    Added basic command args to parse

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+// #[macro_use]
+// extern crate clap;
+// use clap::App;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    println!("
+        ##      ##     #     # ####### #        #####  ####### #     # #######
+        #  #  # #  #  # #  #  # #       #       #     # #     # ##   ## #
+            ##      ##  #  #  # #       #       #       #     # # # # # #
+                        #  #  # #####   #       #       #     # #  #  # #####
+                        #  #  # #       #       #       #     # #     # #
+                        #  #  # #       #       #     # #     # #     # #
+                         ## ##  ####### #######  #####  ####### #     # #######
+
+        #     # ####### ######  #       #######    #     # #     # #     #    #    #     #  ##      ##
+        ##    # #     # #     # #       #          #     # #     # ##   ##   # #   ##    # #  #  # #  #  #
+        # #   # #     # #     # #       #          #     # #     # # # # #  #   #  # #   #     ##      ##
+        #  #  # #     # ######  #       #####      ####### #     # #  #  # #     # #  #  #
+        #   # # #     # #     # #       #          #     # #     # #     # ####### #   # #
+        #    ## #     # #     # #       #          #     # #     # #     # #     # #    ##
+        #     # ####### ######  ####### #######    #     #  #####  #     # #     # #     #
+    ");
+    println!("# This game is simple.");
+    println!("* An interesting random context will be generated. You'll be assigned characters
+            that you'll have to live up to over the course of the conversation");
+
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1].to_string();
+    println!("{:?}", query);
+
+    match query.trim() {
+        "-h" => println!("Yay"),
+        "-qc" => println!("Yay"),
+        "-q" => println!("quit convo"),
+        "-n" => println!("New"),
+        _ => println!("Sad")
+    }
 }
